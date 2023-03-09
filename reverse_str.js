31bc1ffaecfeff8f96f889243bc95dd3657518b8
Matias Molina Bishop <molibi01@gmail.com>
2023-03-09 17:20:01 +0100

    09/03/23 v1.1

--- reverse_str.js
+++ reverse_str.js
@@ -8,3 +8,23 @@ let reverseString = (str) => {
 }
 
 console.log(reverseString('Hi my name is Matias'))
+
+
+const reverseWords = (str) => {
+
+    console.log(str.split('').reverse().join('').split(' ').reverse().join(' '))
+    console.log(str.split(' ').reverse().join(' '))
+
+
+    return str.split('').reverse().join('')
+
+}
+
+console.log(reverseWords('Hi my name is Matias'))
+
+//Depending on what they want as an output try the above solutions 
+// saitaM si eman ym iH
+// iH ym eman si saitaM
+// Matias is name my Hi
+// saitaM si eman ym iH
+//Above are the results of the 4 conosle logs..
