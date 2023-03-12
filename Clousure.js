4929543effe04f9cedaa2bdbc9ed38b2cca30963
Matias Molina Bishop <molibi01@gmail.com>
2023-03-12 17:49:21 +0100

    12/03/23 v1

--- Clousure.js
+++
@@ -1,5 +0,0 @@
-//Create a function which has increment and getValue futionality
-
-//Clousure saving something inside a fucntion after its called?
-
-//NEED TO STUDY CLOSURESS
---
+++ FirstRecuryingCharacter.js
@@ -0,0 +1,36 @@
+//Create a function that accpets an array as argument and retunrs the forst element of that array
+//that repeats its self
+//[2,4,7,2,4,6,9,0,3,5] =======> output 2
+
+const firstDuplicate = (arr) => {
+
+    //I want to ietrate tjroigh this arr and keep track of its eelements
+    //If an elelemnt is reocurrying I want to retunr that elemnet
+    //In order to keep track of these elements and the number of times they appear I can use a hash table
+    //Start by creating an empty oject and adding the elemnts on the arr as keys and the number of times they repar at values
+
+    let obj = {}
+
+    for (let i = 0; i < arr.length; i++) {
+        if (obj[arr[i]] === undefined) {
+            obj[arr[i]] = 1
+        } else {
+            return arr[i]
+        }
+    }
+
+    return 'There are no duplicates on the gieven array'
+}
+
+console.log(firstDuplicate([2, 4, 4, 2, 4, 6, 9, 0, 3, 5]))
+
+
+//WORKING AROUND WITH HASH TABLES
+let obj2 = {
+    name: 'Matias',
+    age: 30
+}
+console.log(obj2)
+obj2['something'] = 'Mercedes'
+console.log(obj2)
+console.log(obj2[4])
