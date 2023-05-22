2766327ff358258a6fe55392cd46954d2fce7622
Matias Molina Bishop <molibi01@gmail.com>
2023-05-22 20:49:16 +0200

    live code practice

--- 7KYU/FirstNonRepeated.js
+++ 7KYU/FirstNonRepeated.js
@@ -3,6 +3,7 @@
 // If there is no unique character, return null in JS or Java, None in Python, '\0' in C.
 // You can assume, that the input string has always non-zero length.
 const firstNonRep = (str) => {
+    //let str = 'hoolh'
     //I need to check the characters and if they are repeated. This means I need to loop over the str
     //I need to save each charater inside an object to check each element as key and the number of times it repeats as their value
     let obj = {}
@@ -15,15 +16,36 @@ const firstNonRep = (str) => {
     }
     console.log(obj)
     let answer
+
     for (const key in obj) {
         if (obj[key] === 1) {
             answer = key
+            break
         } else {
             answer = null
         }
     }
-    console.log(answer)
-    return answer
+
+    //ALLBELOW HERE IS PRATICE WITHIN THIS FUNCTION
+
+    console.log(Object.values(obj))
+    console.log(Object.values(obj).length)
+
+    let allUniqueCheck = Object.values(obj).reduce((acc, currentValue) => {
+        return acc + currentValue
+
+    }, 0)
+    console.log(allUniqueCheck)
+
+    if (allUniqueCheck === Object.values(obj).length) {
+        console.log('if check did happen')
+        answer = str[0]
+    }
+
+
+    ///if (Object.values(obj))
+    //console.log(`THE CHARACTER NOT REPEATED IS: ${answer}`)
+    return `THE CHARACTER NOT REPEATED IS: ${answer}`
     //NOT SURE IF THIS WOULD ENTIRELY WORK AS OBJ ARE NOT ORDERED
 }
 //let str = 'hoolh'
@@ -31,17 +53,17 @@ const firstNonRep = (str) => {
 //OBJECTS are unordered so maybe not the best idea. Lets try a different approach
 //LETS TRY BY LOOPING INISDE A LOOP
 const uniqueCharacter = (str) => {
-    //let unique = false
+
     for (let i = 0; i < str.length; i++) {
         let answer = false
-        //I want to oopp again to compare each letter to to the next ones on the array
+        //I want to loop again to compare each letter to to the next ones on the array
         for (let j = i + 1; j < str.length; j++) {
             if (str[i] === str[j]) {
                 answer = false
                 //break
             } else {
                 answer = true
-                //As long as the word letter or number is nt repeated answer = true
+                //As long as the word letter or number is not repeated answer = true
                 //If this condition holds throought the entire iteration of the second loop it means that index value is unique so we return it
             }
         }
@@ -69,7 +91,7 @@ const youtubeAnser = (str, i, j) => {
                 break
             }
         }
-        //after each index character has been looped and comprad for equality if j === str.length it means that the loop was never broken and therefore that character is unique
+        //after each index character has been looped and compared for equality if j === str.length it means that the loop was never broken and therefore that character is unique
         if (j === str.length)
             return str[i]
     }
@@ -79,3 +101,55 @@ const youtubeAnser = (str, i, j) => {
 console.log(youtubeAnser('aabb', 0, 0))
 
 //I PREFARE THE SECOND PPROACH
+
+console.log('TEST / PTACTICE')
+
+console.log(firstNonRep('hola'))
+
+//TESTING 2ND SOLUTION ON MY OWN DOES NOT WORK!!!!
+
+const loopLopp = (str) => {
+    //I want to loop inside the loop to check each letter with the remaining ones
+    //Initially i set up a condition which will change only if while im looping whatever i specify happens
+    //If a letter loops all the way through the str it means that letter is unique
+
+    let answer = null
+
+
+    for (let i = 0; i < str.length; i++) {
+
+        let check
+
+        //console.log(loopLopp('xhxoolaa'))
+
+        for (let j = i + 1; j < str.length; j++) {
+            console.log(str[i])
+            console.log(str[j])
+            if (str[i] === str[j]) {
+                console.log(str[i], str[j])
+                console.log('BINGBANG!!!!')
+                check = true
+                break
+            }
+            else {
+                console.log('what')
+                console.log(str[i], str[j])
+                check = false
+            }
+        }
+
+        if (check === false) {
+            answer = str[i]
+            console.log('SOOO WHAT?')
+            console.log(`here this letter ${answer} is false?`)
+            //return str[i]
+            break
+        }
+    }
+
+    return answer
+}
+//hollaaah
+console.log('NEW TEST')
+
+console.log(loopLopp('xfxfgxhhooylaa'))
--- 7KYU/RemoveDuplicateWrods.js
+++ 7KYU/RemoveDuplicateWrods.js
@@ -10,3 +10,15 @@ function removeDuplicateWords(string) {
 
     return result.join(' ');
 }
+
+//ANOTERH WAY 
+
+//In this approach, we split the string into an array of words using the split method. 
+//Then, we create a Set from the array to remove duplicate words since Sets only store unique values. 
+//Finally, we convert the Set back to an array using Array.from and join the unique words with spaces using the join method.
+
+function removeDuplicateWords(string) {
+    const words = string.split(' ');
+    const uniqueWords = Array.from(new Set(words));
+    return uniqueWords.join(' ');
+}
